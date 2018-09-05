bf5c7f349aeb2a51e7e137ba05b81053c20c2c7e
forbidnone <43002202+forbidnone@users.noreply.github.com>
2018-09-05 15:38:57 +0100

    Setup project for SFML added while loop and gameWindow

---
+++ Main.cpp
@@ -0,0 +1,26 @@
+// included Libraries
+#include <SFML/Graphics.hpp>
+
+
+// entry point for the program
+int main()
+{
+	
+	// Make a variable called gameWindow of type RenderWindow
+	sf::RenderWindow gameWindow;
+
+	// Game Loop
+	while (true)
+	{
+		
+		// TODO : Check for Input
+
+		// TODO : Update game state
+
+		// TODO : Draw Graphics
+
+
+	}
+	// exit point for the program
+	return 0;
+}
