b5ad7b4e31e1afb78922acf6c9af7f8af3516b2e
forbidnone <43002202+forbidnone@users.noreply.github.com>
2018-09-10 15:53:49 +0100

    Added Button to project and Began drawing button

--- Main.cpp
+++ Main.cpp
@@ -5,14 +5,32 @@
 // entry point for the program
 int main()
 {
-	
+	//-----------------------------------------------------------------------------------
+	// Game Setup
+	// ----------------------------------------------------------------------------------
+
+
 	// Make a variable called gameWindow of type RenderWindow
 	sf::RenderWindow gameWindow;
 	gameWindow.create(sf::VideoMode::getDesktopMode(), "Button Masher",
 		sf::Style::Titlebar | sf::Style::Close);
 
 
+	// Create Button Spirte
+	sf::Texture buttonTexture;
+	buttonTexture.loadFromFile("graphics/button.png");
+
+	sf::Sprite buttonSprite;
+	buttonSprite.setTexture(buttonTexture);
+	buttonSprite.setPosition(
+		gameWindow.getSize().x / 2 - buttonTexture.getSize().x / 2,
+		gameWindow.getSize().y / 2 - buttonTexture.getSize().y / 2
+	);
+
+
+	//-----------------------------------------------------------------------------------
 	// Game Loop
+	//-----------------------------------------------------------------------------------
 	// Runs every frame until the game window is closed
 	while (gameWindow.isOpen())
 	{
@@ -39,6 +57,18 @@ int main()
 
 		// TODO : Draw Graphics
 
+		// Clear the window to a single colour
+		gameWindow.clear(sf::Color::Black);
+
+
+		// Draw everything
+		gameWindow.draw(buttonSprite);
+
+
+		// Display the window contents on the screen
+		gameWindow.display();
+
+		
 
 	}
 	// exit point for the program
